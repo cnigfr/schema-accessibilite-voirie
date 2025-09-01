5d810305b0b96e27a641912ed198afa9031596aa
GT CNIG DDU <79853274+GT-CNIG-DDU@users.noreply.github.com>
2025-09-01 16:14:29 +0200

    maj gabarit

---
+++ Standard/Gabarit geopackage/batch.sh
@@ -0,0 +1,30 @@
+# !/bin/bash
+# Génère le gabarit gpkg à partir :
+# - du fichier GPKG vide : gpkg_vide.gpkg
+# - du fichier SQL template de génération des tables du standard : standard_cnig_accessibilite.sql
+
+path='D:\Park\ATELIER_SQL'
+
+echo "-------------------------------------------------"
+echo "Genération du gabarit GPKG à partir du code SQL  "
+echo "-------------------------------------------------"
+
+cd "$path"
+
+# PRODUCTION
+echo "   Exécution du SQL de production"
+echo "-------------------------------------------------"
+"$path\SQLITE\sqlite3.exe" gabarit_cnig_accessibilite.gpkg < script_gabarit_CNIG_Accessibilité.sql
+echo "   Exécution du SQL de production : effectuée !"
+echo "-------------------------------------------------"
+
+# TEST
+# echo "   Exécution du SQL de TEST"
+# echo "-------------------------------------------------"
+# "$path\SQLITE\sqlite3.exe" gabarit_cnig_accessibilite_TEST.gpkg < script_gabarit_CNIG_Accessibilité_TEST.sql
+# echo "   Exécution du SQL de TEST : effectuée !
+# "echo "-------------------------------------------------"
+
+echo "Appuyer sur Entrée pour continuer..."
+read a
+
